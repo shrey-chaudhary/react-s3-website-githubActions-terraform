1f423028d405201e88ac7fadc1adeebd784288d0
shre0873 <shrey.chaudhary@rackspace.com>
2021-12-09 19:22:45 +0530

    test commit

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 id="heading">A simple boilerplate for ReactJS Website hosted on S3 bucket and deployed using terrafomr and Github Actions</h1>
+        <h1 id="heading">Simple boilerplate for ReactJS Website hosted on S3 bucket and deployed using terrafomr and Github Actions</h1>
       </header>
     </div>
   );
